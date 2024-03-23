73a2793531653678df45d1cf95856a8cebaadf66
Link Hsu <linkhsu@Links-MacBook-Pro.local>
2024-03-24 03:18:44 +0800

    add jpg background-image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
 import logo from './logo.png';
-import background1 from './backgroundImage-one.png';
-import background2 from './backgroundImage-two.png';
-import background3 from './backgroundImage-three.png';
-import background4 from './backgroundImage-four.png';
+import background1 from './backgroundImage-one.jpg';
+import background2 from './backgroundImage-two.jpg';
+import background3 from './backgroundImage-three.jpg';
+import background4 from './backgroundImage-four.jpg';
 import memberBackground1 from './meber-background-1.jpg';
 // import memberBackground2 from './meber-background-2.jpg';
 import lineLoginButton from './btn_login_base.png';
